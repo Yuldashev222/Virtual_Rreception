8c53c3e89783ba45a9300c2089aa158450101b61
Yuldashev222 <oybekyuldashov54@gmail.com>
2022-03-03 14:09:58 +0500

    form tugadi

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const firstNextBtn = document.querySelector(".next_btn");
 const prevBtnSecond = document.querySelector(".prev_1");
 const nextBtnSecond = document.querySelector(".next_1");
 const prevBtnThird = document.querySelector(".prev_2");
-const nextBtnThird = document.querySelector(".next_2");
-const prevBtnFourth = document.querySelector(".prev_3");
+
+const organization = document.querySelector(".organization");
+const organizationName = document.querySelectorAll(".organization__name");
 
 const submitBtn = document.querySelector(".submit");
 
@@ -14,7 +15,7 @@ const progressCheck = document.querySelectorAll(".step .check");
 
 const bullet = document.querySelectorAll(".step .bullet");
 
-let max = 4;
+let max = 3;
 let current = 1;
 
 firstNextBtn.addEventListener("click", function(){
@@ -28,15 +29,6 @@ firstNextBtn.addEventListener("click", function(){
 
 nextBtnSecond.addEventListener("click", function(){
   slidePage.style.marginLeft = "-50%";
-
-  bullet[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  current += 1;
-});
-
-nextBtnThird.addEventListener("click", function(){
-  slidePage.style.marginLeft = "-75%";
 
   bullet[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -68,15 +60,6 @@ prevBtnSecond.addEventListener("click", function(){
 
 prevBtnThird.addEventListener("click", function(){
   slidePage.style.marginLeft = "-25%";
-
-  bullet[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  current -= 1;
-});
-
-prevBtnFourth.addEventListener("click", function(){
-  slidePage.style.marginLeft = "-50%";
 
   bullet[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
